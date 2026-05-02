4f9fabd13fcee82b09932657644256cd73419f97
guillaume.biondo <guillaume.biondo@gmail.com>
2026-05-02 18:36:00 +0200

    fix: expose mail error in bug-report response for diagnosis

--- backend/app/Http/Controllers/Api/BugReportController.php
+++ backend/app/Http/Controllers/Api/BugReportController.php
@@ -15,15 +15,22 @@ class BugReportController extends Controller
         $user = $request->user();
         $data = $request->validated();
 
-        Mail::to('bot@guigeek.dev')->send(new BugReportMail(
-            userName:      $user->name,
-            userEmail:     $user->email,
-            message:       $data['message'],
-            url:           $data['url'],
-            apiCalls:      $data['api_calls'] ?? [],
-            consoleErrors: $data['console_errors'] ?? [],
-            reportedAt:    now()->format('d/m/Y H:i:s'),
-        ));
+        try {
+            Mail::to('bot@guigeek.dev')->send(new BugReportMail(
+                userName:      $user->name,
+                userEmail:     $user->email,
+                message:       $data['message'],
+                url:           $data['url'],
+                apiCalls:      $data['api_calls'] ?? [],
+                consoleErrors: $data['console_errors'] ?? [],
+                reportedAt:    now()->format('d/m/Y H:i:s'),
+            ));
+        } catch (\Throwable $e) {
+            return response()->json([
+                'message' => 'Erreur mail : ' . $e->getMessage(),
+                'class'   => get_class($e),
+            ], 500);
+        }
 
         return response()->json(['message' => 'Rapport envoyé.']);
     }
